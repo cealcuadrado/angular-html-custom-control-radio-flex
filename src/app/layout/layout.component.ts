297841b0d93ee7209ff291e88bf6af07c30a0b11
CeAlCuadrado <cealcuadrado@outlook.com>
2021-05-09 14:11:02 -0400

    Primer commit

---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss']
+})
+export class LayoutComponent implements OnInit {
+
+  form: FormGroup;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      name: new FormControl('', Validators.required),
+      gender: new FormControl('', Validators.required)
+    });
+  }
+
+  submit() {
+    console.log(this.form.value);
+  }
+
+}
